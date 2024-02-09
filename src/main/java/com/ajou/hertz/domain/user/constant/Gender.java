16ecb4dc9717f706afcc216320ea86987e3bde52
Wo-ogie <siwall0105@gmail.com>
2024-02-09 22:59:12 +0900

    feat: #7 user entity 구현

---
+++ src/main/java/com/ajou/hertz/domain/user/constant/Gender.java
@@ -0,0 +1,8 @@
+package com.ajou.hertz.domain.user.constant;
+
+public enum Gender {
+
+	MALE,
+	FEMALE,
+	ETC
+}
---
+++ src/main/java/com/ajou/hertz/domain/user/entity/User.java
@@ -0,0 +1,64 @@
+package com.ajou.hertz.domain.user.entity;
+
+import java.time.LocalDate;
+
+import org.springframework.lang.NonNull;
+
+import com.ajou.hertz.domain.user.constant.Gender;
+import com.ajou.hertz.global.common.entity.TimeTrackedBaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "users")
+@Entity
+public class User extends TimeTrackedBaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable = false, updatable = false)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(unique = true)
+	private String kakaoUid;
+
+	private LocalDate birth;
+
+	@Column(nullable = false)
+	private Gender gender;
+
+	@Column(unique = true)
+	private String phone;
+
+	private String contactLink;
+
+	@NonNull
+	public static User create(
+		@NonNull String email,
+		@NonNull String password,
+		String kakaoUid,
+		LocalDate birth,
+		@NonNull Gender gender,
+		String phone,
+		String contactLink
+	) {
+		return new User(null, email, password, kakaoUid, birth, gender, phone, contactLink);
+	}
+}
