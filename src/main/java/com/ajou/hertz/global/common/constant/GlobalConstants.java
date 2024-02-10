295edfc5192514d8d7724db82e50740f5924ccc6
Wo-ogie <siwall0105@gmail.com>
2024-02-10 13:45:25 +0900

    feat: #7 API minor version 설정을 위한 header name 정의

---
+++ src/main/java/com/ajou/hertz/global/common/constant/GlobalConstants.java
@@ -0,0 +1,6 @@
+package com.ajou.hertz.global.common.constant;
+
+public final class GlobalConstants {
+
+	public static final String API_MINOR_VERSION_HEADER_PREFIX = "Hertz-API-Minor-Version=";
+}
