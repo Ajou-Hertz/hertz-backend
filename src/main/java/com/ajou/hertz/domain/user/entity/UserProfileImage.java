e4c99e796b66a1c1cb9d24d39d69fb7500b8c5c8
Wo-ogie <siwall0105@gmail.com>
2024-02-18 14:58:45 +0900

    feat: #30 유저 프로필 이미지 entity 구현

---
+++ src/main/java/com/ajou/hertz/domain/user/entity/UserProfileImage.java
@@ -0,0 +1,36 @@
+package com.ajou.hertz.domain.user.entity;
+
+import com.ajou.hertz.common.entity.FileEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class UserProfileImage extends FileEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_profile_image_id", nullable = false)
+	private Long id;
+
+	@JoinColumn(name = "user_id", nullable = false)
+	@OneToOne(fetch = FetchType.LAZY)
+	private User user;
+
+	private UserProfileImage(Long id, User user, String originalName, String storedName, String url) {
+		super(originalName, storedName, url);
+		this.id = id;
+		this.user = user;
+	}
+}
