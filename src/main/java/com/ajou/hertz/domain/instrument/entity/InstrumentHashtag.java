3b39bbcb5e299d13e9ed5b2d8604014ff18ebc87
Wo-ogie <siwall0105@gmail.com>
2024-02-26 02:05:56 +0900

    feat: #46 악기 해시태그 entity 구현

---
+++ src/main/java/com/ajou/hertz/domain/instrument/entity/InstrumentHashtag.java
@@ -0,0 +1,35 @@
+package com.ajou.hertz.domain.instrument.entity;
+
+import com.ajou.hertz.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class InstrumentHashtag extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "instrument_hashtag_id", nullable = false)
+	private Long id;
+
+	@JoinColumn(name = "instrument_id", nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Instrument instrument;
+
+	@Column(length = 10, nullable = false)
+	private String content;
+}
