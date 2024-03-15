f33823fd68b09e6448cd32ba89ddb5fb703200c4
Wo-ogie <siwall0105@gmail.com>
2024-03-15 19:46:43 +0900

    feat: #83 AWS S3 복수 파일 삭제 기능 구현

--- src/test/java/com/ajou/hertz/unit/common/file/service/AmazonS3FileServiceTest.java
+++ src/test/java/com/ajou/hertz/unit/common/file/service/AmazonS3FileServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.BDDMockito.*;
 import java.io.IOException;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,6 +23,7 @@ import com.ajou.hertz.common.file.dto.FileDto;
 import com.ajou.hertz.common.file.service.AmazonS3FileService;
 import com.ajou.hertz.common.properties.AWSProperties;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
 
@@ -42,17 +44,17 @@ class AmazonS3FileServiceTest {
 	@Mock
 	private AWSProperties.CloudFront cloudFront;
 
-	private void setUpMockProperties() {
-		given(awsProperties.s3()).willReturn(s3);
-		given(awsProperties.cloudFront()).willReturn(cloudFront);
-		given(awsProperties.s3().bucketName()).willReturn("aws-s3-bucket-name");
-		given(awsProperties.cloudFront().baseUrl()).willReturn("aws-cloud-front-base-url/");
+	@BeforeEach
+	void setUpMockProperties() {
+		lenient().when(awsProperties.s3()).thenReturn(s3);
+		lenient().when(awsProperties.cloudFront()).thenReturn(cloudFront);
+		lenient().when(awsProperties.s3().bucketName()).thenReturn("aws-s3-bucket-name");
+		lenient().when(awsProperties.cloudFront().baseUrl()).thenReturn("aws-cloud-front-base-url/");
 	}
 
 	@Test
 	void 파일이_주어지면_파일을_S3에_업로드한다() {
 		// given
-		setUpMockProperties();
 		MultipartFile multipartFile = createMockMultipartFile(null);
 		given(s3Client.putObject(any(PutObjectRequest.class))).willReturn(new PutObjectResult());
 
@@ -61,7 +63,7 @@ class AmazonS3FileServiceTest {
 
 		// then
 		then(s3Client).should().putObject(any(PutObjectRequest.class));
-		then(s3Client).shouldHaveNoMoreInteractions();
+		verifyEveryMocksShouldHaveNoMoreInteractions();
 	}
 
 	@Test
@@ -75,13 +77,12 @@ class AmazonS3FileServiceTest {
 
 		// then
 		assertThat(t).isInstanceOf(MultipartFileNotReadableException.class);
-		then(s3Client).shouldHaveNoInteractions();
+		verifyEveryMocksShouldHaveNoMoreInteractions();
 	}
 
 	@Test
 	void 여러_개의_파일들이_주어지면_전부_S3에_업로드한다() {
 		// given
-		setUpMockProperties();
 		List<MultipartFile> multipartFiles = List.of(
 			createMockMultipartFile("1"),
 			createMockMultipartFile("2"),
@@ -94,13 +95,36 @@ class AmazonS3FileServiceTest {
 
 		// then
 		verify(s3Client, times(multipartFiles.size())).putObject(any(PutObjectRequest.class));
-		then(s3Client).shouldHaveNoMoreInteractions();
+		verifyEveryMocksShouldHaveNoMoreInteractions();
 		assertThat(results.size()).isEqualTo(multipartFiles.size());
 		for (int i = 0; i < results.size(); i++) {
 			MultipartFile multipartFile = multipartFiles.get(i);
 			FileDto result = results.get(i);
 			assertThat(result.getOriginalName()).isEqualTo(multipartFile.getOriginalFilename());
 		}
+	}
+
+	@Test
+	void S3에_저장된_파일들의_이름이_주어지고_파일들을_S3_버킷에서_삭제한다() throws Exception {
+		// given
+		List<String> storedFileNames = List.of(
+			"stored-file-name-1",
+			"stored-file-name-2",
+			"stored-file-name-3",
+			"stored-file-name-4"
+		);
+		willDoNothing().given(s3Client).deleteObject(any(DeleteObjectRequest.class));
+
+		// when
+		sut.deleteAll(storedFileNames);
+
+		// then
+		verify(s3Client, times(storedFileNames.size())).deleteObject(any(DeleteObjectRequest.class));
+		verifyEveryMocksShouldHaveNoMoreInteractions();
+	}
+
+	private void verifyEveryMocksShouldHaveNoMoreInteractions() {
+		then(s3Client).shouldHaveNoMoreInteractions();
 	}
 
 	private MockMultipartFile createMockMultipartFile(String fileName) {
