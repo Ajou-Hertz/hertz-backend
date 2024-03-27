2b53a1f69a49d1ae13d30554a2aa1e2cfb156b65
tinon1004 <tinon1004@ajou.ac.kr>
2024-03-28 00:28:55 +0900

    feat: #89 판매자 정보 조회 API 구현

--- src/main/java/com/ajou/hertz/domain/instrument/repository/InstrumentRepository.java
+++ src/main/java/com/ajou/hertz/domain/instrument/repository/InstrumentRepository.java
@@ -2,7 +2,11 @@ package com.ajou.hertz.domain.instrument.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ajou.hertz.domain.instrument.constant.InstrumentProgressStatus;
 import com.ajou.hertz.domain.instrument.entity.Instrument;
 
 public interface InstrumentRepository extends JpaRepository<Instrument, Long>, InstrumentRepositoryCustom {
+
+	long countBySellerIdAndProgressStatus(Long sellerid, InstrumentProgressStatus progressStatus);
+
 }
